c1cbe86d546b65b0f32192a196881de0a1d01f6e
Yero-des <yero_zc@hotmail.com>
2023-10-03 16:43:13 -0500

    using my first setState

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,46 +1,113 @@
 import 'package:flutter/material.dart';
 
+// Declarar variables
+TextStyle customFontWeight ({ double fontSize = 15 }) {
+  return TextStyle(
+    color: Colors.white, //
+    fontSize: fontSize,
+    fontWeight: FontWeight.bold,
+  );
+}
+
 void main() => runApp(const HomeScreen());
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int contador = 0;
+
+  void zeroCounter() {
+    setState(() {
+      contador = 0;
+    });
+  }
+
+  void incrementCounter() {
+    setState(() {
+      contador++;
+    });
+  }
+
+  void decrementCounter() {
+    setState(() {
+      contador--;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+
+      // Bar 
       appBar: AppBar(
         title: const Text(
-          "Titulo de la app",
+          "AddPress",
           style: TextStyle(
             color: Colors.black,
             fontWeight: FontWeight.bold
           ),
         ),
         elevation: 10,
-      ),
-      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-      body: const Center(
+      ), 
+
+      // Body
+      body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center, // Center content
+          crossAxisAlignment: CrossAxisAlignment.center, // Center items
           children: <Widget> [
-            Text("Click para cambiar los datos del contador", style: TextStyle(
-                color: Colors.white, // Cambia el color
-                fontSize: 15, // Tamaño de fuente opcional
-                fontWeight: FontWeight.bold, // Peso de fuente opcional
-              ),
-            ), 
-            Text("20", style: TextStyle(
-                color: Colors.white, // Cambia el color
-                fontSize: 30, // Tamaño de fuente opcional
-                fontWeight: FontWeight.bold, // Peso de fuente opcional
-              ),
-            ),
+            Text("Click para cambiar los datos del contador", style: customFontWeight( fontSize: 15 )), 
+            const SizedBox(height: 30),
+            Text('$contador', style: customFontWeight(fontSize: 30)),
           ],
         ),
-      )
+      ),
+
+      floatingActionButton: Row (
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget> [
+
+          FloatingActionButton(
+            onPressed: () {
+              zeroCounter();
+            },
+            child: const Icon(Icons.repeat_rounded),
+          ),
+
+          const SizedBox(width: 20),
+
+          FloatingActionButton(
+            onPressed: () {
+              decrementCounter();
+            },
+            child: const Icon(Icons.exposure_minus_1),
+          ),
+
+          const SizedBox(width: 20),
+
+          FloatingActionButton(
+            onPressed: () { 
+              incrementCounter();
+            },
+            child: const Icon(Icons.plus_one),
+          ),
+
+        ]
+      ),
+      
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+
+      // Brackground
+      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+
     );
   }
-
 }
